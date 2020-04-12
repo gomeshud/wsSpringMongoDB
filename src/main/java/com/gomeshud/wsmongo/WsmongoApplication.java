c934ac7b6f2b4c5040c47f2c8ccb89290c4d56a5
gomeshud <hgf1@ifal.edu.br>
2020-04-11 22:57:10 -0300

    Primeiro commit - criando projeto

---
+++ src/main/java/com/gomeshud/wsmongo/WsmongoApplication.java
@@ -0,0 +1,13 @@
+package com.gomeshud.wsmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsmongoApplication.class, args);
+	}
+
+}
